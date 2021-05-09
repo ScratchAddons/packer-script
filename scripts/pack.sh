935f3ff9b8a9eeaf1938f9b9cc78d58e1a238acd
apple502j <33279053+apple502j@users.noreply.github.com>
2021-05-09 13:37:47 +0900

    Forgot to -r it

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -8,7 +8,7 @@ echo $(printf "Packing build for %s" $ENVIRONMENT)
 node $GITHUB_ACTION_PATH/scripts/manifest.js
 mkdir $GITHUB_WORKSPACE/.dist 2>/dev/null
 
-VERSION_NAME=$(cat $GITHUB_WORKSPACE/manifest.json | jq ".version_name" | sed "s/[.-]/_/g;s/prerelease/pre/")
+VERSION_NAME=$(cat $GITHUB_WORKSPACE/manifest.json | jq -r ".version_name" | sed "s/[.-]/_/g;s/prerelease/pre/")
 FILENAME=$(printf "ScratchAddons-%s-%s.zip" $VERSION_NAME $ENVIRONMENT) 
 
 git archive --format=zip -o $GITHUB_WORKSPACE/.dist/$FILENAME HEAD
